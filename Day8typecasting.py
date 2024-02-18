f347d528f354d4e6714c81df6e2ce2cd798b8b6f
Ashis Subedi <73783936+AshisSubedi07@users.noreply.github.com>
2024-02-18 07:00:28 -0800

    Add files via upload

---
+++ Day8typecasting.py
@@ -0,0 +1,12 @@
+#What is typecasting in Python?
+#Type Casting is the method to convert the Python variable datatype into a certain data type in order to perform the required operation by users.
+#Basic program concept of typecasting in python
+#def string
+string = "10" 
+#Def integer
+num=-3 
+#converting string to integer
+string_number = int(string) 
+#Adding the integer num with string
+sum=num + string_number 
+print(sum) #Printing the added number
